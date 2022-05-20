f0cb4e73255edfdd33c96b0d36ccad543aed30bb
LeeJaeLee <cuonggaayf01@gmail.com>
2022-05-20 19:52:37 +0700

    PTIT

---
+++ src/main/java/dbHelper/DatabaseHelper.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dbHelper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author LeeJaeLee
+ */
+public class DatabaseHelper {
+    public static Connection getConnection() throws Exception{
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String url = "jdbc:sqlserver://localhost:1433;databaseName=shopLaptop";
+        String user = "sa";
+        String pass = "261001";
+        Connection con = DriverManager.getConnection(url, user, pass);
+        return con;
+    }
+}
